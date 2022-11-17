f51040d66952eab78c43e71ac2986e7a42248f3b
Santo Khan <santokhan1999@gmail.com>
2022-11-17 17:21:16 +0600

    update form

--- assets/js/form.js
+++ assets/js/form.js
@@ -23,17 +23,5 @@ function submitForm() {
     screenRecorderFunc(data);
   });
 }
-// remove this invocation on production
-hideFormShowWhiteBoard();
-const data = {
-  title: "Paragraph a English Language Course Secondary",
-  paragraph: `
-(123) 456-7890
-NO_REPLY@EXAMPLE.COM
-YOUR CITY, ST 12345
-September 04, 20XX
-Dear Ms. Reader,
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
-veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.`,
-};
-startTyping(data);
+
+
